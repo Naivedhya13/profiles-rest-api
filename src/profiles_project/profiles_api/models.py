a390f71f813fbfbfefa5fafaba2e173f3452ea55
Naivedhya13 <44753319+Naivedhya13@users.noreply.github.com>
2021-08-24 01:45:29 +0200

    Added a custom user profile model, a manager, and the migrations for our database.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -18,7 +18,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -30,7 +30,7 @@ class UserProfileManager(BaseUserManager):
             user.is_superuser= True
             user.is_staff= True
 
-            user.save(using=self.db)
+            user.save(using=self._db)
 
             return user
 
